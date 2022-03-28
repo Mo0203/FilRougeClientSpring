5dca9a48e1d68c08afc52df9c551d50813905154
JesusWasTaken <heard.philippe@gmail.com>
2022-03-28 16:25:21 +0200

    init

---
+++ apiinsertion/src/main/java/com/fr/apiinsertion/ApiinsertionApplication.java
@@ -0,0 +1,13 @@
+package com.fr.apiinsertion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiinsertionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiinsertionApplication.class, args);
+	}
+
+}
---
+++ apiinsertion/src/test/java/com/fr/apiinsertion/ApiinsertionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fr.apiinsertion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiinsertionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
